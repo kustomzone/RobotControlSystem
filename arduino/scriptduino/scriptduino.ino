2226c8d67e398711c8b4d60d794664e9ae1ca4e6
Tuna Oezer <hacker@tunaoezer.com>
2013-05-17 07:39:53 -0700

    Adding support for PWM and extended Serial ports.

--- arduino/scriptduino/scriptduino.ino
+++ arduino/scriptduino/scriptduino.ino
@@ -15,14 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define VERSION "ScriptDuino 0.1 [2013.05.13.00]"
+#define VERSION "ScriptDuino 0.1 [2013.05.17.00]"
 
-//#define BOARD "Arduino Mega 2560"
-#define BOARD "Arduino BT 328"
-//#define BAUD_RATE 9600
-#define BAUD_RATE 115200
+#define BOARD "Arduino Mega 2560"
+//#define BOARD "Arduino BT 328"
+#define BAUD_RATE 9600
+//#define BAUD_RATE 115200
 #define MAX_INPUT_SIZE 512
-#define NUM_FUNCTIONS 4
+#define NUM_FUNCTIONS 5
 
 boolean echo;
 
@@ -104,6 +104,7 @@ class Reset : public Function {
       for (int pin = 0; pin < NUM_DIGITAL_PINS; ++pin) {
         digitalWrite(pin, LOW);
       }
+      return true;
     }
 };
 
@@ -128,25 +129,45 @@ class DigitalWrite : public Function {
     }
 };
 
+class Pwm : public Function {
+  public:
+    Pwm()
+      : Function("pwm") {
+    }
+    
+    virtual boolean exec(String* args) const {
+      int pin = 0;
+      int value = 0;
+      popInteger(args, &pin);
+      popInteger(args, &value);
+      if (pin < 0 || pin >= NUM_DIGITAL_PINS) return false;
+      analogWrite(pin, value);
+      return true;
+    }
+};
+
 String input;
 Function* functions[NUM_FUNCTIONS];
 
 void setup() {
+  TCCR1B = 1;
+
   for (int pin = 0; pin < NUM_DIGITAL_PINS; ++pin) {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
   input = "";
   functions[0] = new DigitalWrite();
-  functions[1] = new Ping();
-  functions[2] = new Echo();
-  functions[3] = new Reset();
+  functions[1] = new Pwm();
+  functions[2] = new Ping();
+  functions[3] = new Echo();
+  functions[4] = new Reset();
   echo = false;
   
   Serial.begin(BAUD_RATE);
   while (!Serial);
-  
-  Serial.println(VERSION);
+  Serial2.begin(115200);
+  while (!Serial2);
 }
 
 boolean exec(const String& func_name, String* args) {
@@ -162,6 +183,7 @@ void loop() {
   if (input.length() >= MAX_INPUT_SIZE) input = "";
   int index = input.indexOf(';');
   if (index >= 0) {
+    if (echo) Serial.println(input);
     String func_str = input.substring(0, index);
     index++;
     if (input.length() > index) {
@@ -186,6 +208,27 @@ void loop() {
 void serialEvent() {
   while (Serial.available() && input.length() < MAX_INPUT_SIZE) {
     char ch = (char) Serial.read();
+    input += ch;
+  }
+}
+
+void serialEvent1() {
+  while (Serial1.available() && input.length() < MAX_INPUT_SIZE) {
+    char ch = (char) Serial1.read();
+    input += ch;
+  }
+}
+
+void serialEvent2() {
+  while (Serial2.available() && input.length() < MAX_INPUT_SIZE) {
+    char ch = (char) Serial2.read();
+    input += ch;
+  }
+}
+
+void serialEvent3() {
+  while (Serial3.available() && input.length() < MAX_INPUT_SIZE) {
+    char ch = (char) Serial3.read();
     input += ch;
   }
 }
